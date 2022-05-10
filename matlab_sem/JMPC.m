4bdec31656c2703873419b9c7f987fe0f180c46c
kipariss1 <kpassudin@gmail.com>
2022-05-10 18:52:36 +0200

    MPC worksgit add .

--- matlab_sem/JMPC.m
+++ matlab_sem/JMPC.m
@@ -1,4 +1,4 @@
-function J=JMPC(x0,up,A,B,Q,R,M,W)
+function J=JMPC(x0,up,sp,A,B,Q,R,M,W)
 % evaluate basic quadratic criterion 
 % on prediction horizon with discrete-time lin. model
 % with LQR terminal penalty
@@ -7,16 +7,26 @@ Nu=size(B,2);
 N=length(up)/Nu;
 
 J=0;
-x=x0;
+x=x0;           % initial x
+e = sp - x;     % initial error
+
 for k=0:N-1
     
     u=up(k+1);  % uk
     
-    J=J+x'*Q*x+u'*R*u+2*x'*M*u; % sum
+%     if k>=1
+%         delta_u = u - up(k);
+%     else 
+%         delta_u = 0;
+%     end
+        
+    J=J+e'*Q*e+u'*R*u+2*x'*M*u; % sum
     
     x=A*x+B*u;  % xk+1
+    
+    e = sp - x; % e[k+1]
 
 end
 
-J=J+x'*W*x; % xN
+J=J+e'*W*e; % xN
 
